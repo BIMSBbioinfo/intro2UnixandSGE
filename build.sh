3cbb8f73ca47ca20142d1c0e94e549490b98bb1f
Ricardo Wurmus <rekado@elephly.net>
2021-03-03 13:35:20 +0100

    Better deal with sub-directories.

--- build.sh
+++ build.sh
@@ -7,30 +7,28 @@ pandoc -f markdown -t html SUMMARY.md | \
 
 echo '[' > tmp/documents.json
 
-for directory in $(ls -1 pages); do
-    for file in $(ls -1 pages/$directory); do
-        echo $file
-        title=$(egrep --max-count=1 '^# ' pages/$directory/$file | tail -c +3)
+for file in $(find pages -type f -printf '%P\n'); do
+    echo $file
+    title=$(egrep --max-count=1 '^# ' pages/$file | tail -c +3)
 
-        # Build JSON documents for indexing
-        tr \" \' < pages/$directory/$file | tr -d "[:punct:]" | \
-            pandoc -f gfm \
-                   -t plain \
-                   -V url="$directory/$(basename $file .md).html" \
-                   -V pagetitle="$title" \
-                   --template template.json - | tr "\n" " " >> tmp/documents.json
-
-        # Build web pages
-        mkdir -p book/$directory
-        # TODO: level should be taken from SUMMARY
-        pandoc -f markdown_github+smart \
-               -t html \
-               -V level="1.1" \
-               -V navigation="$(cat book/SUMMARY.html)" \
+    # Build JSON documents for indexing
+    tr \" \' < pages/$file | tr -d "[:punct:]" | \
+        pandoc -f gfm \
+               -t plain \
+               -V url="$(dirname $file)/$(basename $file .md).html" \
                -V pagetitle="$title" \
-               -o "book/$directory/$(basename $file .md).html" \
-               --template template.html pages/$directory/$file
-    done
+               --template template.json - | tr "\n" " " >> tmp/documents.json
+
+    # Build web pages
+    mkdir -p book/$(dirname $file)
+    # TODO: level should be taken from SUMMARY
+    pandoc -f markdown_github+smart \
+           -t html \
+           -V level="1.1" \
+           -V navigation="$(cat book/SUMMARY.html)" \
+           -V pagetitle="$title" \
+           -o "book/$(dirname $file)/$(basename $file .md).html" \
+           --template template.html pages/$file
 done
 echo '{}]' >> tmp/documents.json
 
